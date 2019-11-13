4448022efae7d61fef38a87c95faaa00a7c00eff
Jon Maldia <jon@maldia.net>
2019-11-12 20:10:39 -0500

    Initial App Setup

---
+++ app.js
@@ -0,0 +1,21 @@
+let bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    express     = require("express"),
+
+    app         = expresss();
+
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); // to be able to serve up assets
+
+
+
+
+
+// Tell express to listen for requests -start server 
+// start your app with this command: PORT=3001 nodemon app.js
+app.listen(process.env.PORT, process.env.IP, () =>{
+    console.log("Blog App Server has started")
+});
