d4ec7edbdd223ddfb78600dc395c4aa53ebb32cc
Jon Maldia <jon@maldia.net>
2019-11-13 17:51:33 -0500

    Create a delete route

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-let bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    express     = require("express"),
+let bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require("mongoose"),
+    express         = require("express"),
 
-    app         = express();
+    app             = express();
 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); // to be able to serve up assets
+app.use(methodOverride("_method"));
 
 
 
@@ -84,9 +86,20 @@ app.get("/blogs/:id/edit", (req, res) => {
 // EDIT ROUTE PUT - edit blog details
 app.put("/blogs/:id", (req, res) => {
     // edit post
-    Blog.create(req.body.blog, (err, newBlog) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
         if (err) {
-            res.render("new");
+            res.redirect("/blogs");
+        } else {
+            res.redirect(`/blogs/${req.params.id}`);
+        }
+    });
+});
+// DELETE ROUTE - delete blog post
+app.delete("/blogs/:id", (req, res) => {
+    // edit post
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            res.redirect("/blogs");
         } else {
             res.redirect("/blogs");
         }
